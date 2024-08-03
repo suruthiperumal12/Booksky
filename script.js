5005ededbf0759ed30ec3e8bb16aa307fc67aa5f
suruthiperumal12 <suruthi12jan@gmail.com>
2024-08-03 20:58:53 +0530

    modified files

---
+++ script.js
@@ -0,0 +1,63 @@
+
+//selecting add popup button
+var plusbtn=document.getElementById("add-popup-btn")
+//selecting popupbox
+var popupbox=document.querySelector(".popup-box")
+//selecting overlay
+var popupoverlay=document.querySelector(".popup-overlay")
+plusbtn.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupbox.style.display="block"
+})
+//selecting cancel button
+var cancelbook =document.getElementById("cancel-book")
+cancelbook.addEventListener('click',function(event){
+    event.preventDefault()
+    popupoverlay.style.display="none"
+    popupbox.style.display="none"
+})
+// //selecting book container
+var container=document.querySelector(".container")
+//selecting add button,input,textarea
+var addbook=document.getElementById("add-book")
+var inputtitle=document.getElementById("title")
+var inputauthor=document.getElementById("author")
+var textarea=document.getElementById("description")
+addbook.addEventListener('click',function(event){
+    event.preventDefault() ;
+    //creating div tag
+    var div=document.createElement("div");
+    div.setAttribute("class","book-container");
+    div.innerHTML=`<h2>${inputtitle.value}</h2>
+    <h5>${inputauthor.value}</h5>
+    <p>${textarea.value}</p>
+    <button onclick="deletebook(event)">Delete</button>`
+    //append the div tag inside the container tag
+    container.append(div)
+    popupoverlay.style.display="none"
+    popupbox.style.display="none"
+
+    // // another logic for creating bookcontainertags
+    // div.innerHTML="<h2>"+inputtitle.value+"</h2>"
+    // +"<h5>"+inputauthor.value+"</h5>"
+    // +"<h2>"+textarea.value+"</h2>"+
+    // "<button onclick='deletebook(event)'>Delete</button>"
+    //  popupoverlay.style.display="none"
+    //  popupbox.style.display="none"
+    //  container.append(div)
+    // //  another logics
+    //  var h2=document.createElement('h2')
+    //  h2.textContent=inputtitle.value 
+    //  div.append(h2)
+    //  container.append(div)
+
+    //clearing the div tag fields
+    inputtitle.value=''
+    inputauthor.value=''
+    textarea.value=''
+})
+var deletebtn=document.getElementById("delete-btn")
+function deletebook(event)
+{
+    event.target.parentElement.remove()
+}
